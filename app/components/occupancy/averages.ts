120115b36a9dd74105c2f9f3218f13c65953f655
Bas Kamer <bas@bushbaby.nl>
2023-07-28 12:36:28 +0200

    refactor: removed ember-moment, moment-js and monent-timezone

--- app/components/occupancy/averages.ts
+++ app/components/occupancy/averages.ts
@@ -1,18 +1,23 @@
-import Component from '@glimmer/component';
-import moment from 'moment';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import { isNone } from '@ember/utils';
+import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 
-export default class OccupancyAveragesComponent extends Component {
-  @service intl;
+interface Args {
+  occupancies: any[];
+}
+
+const now = new Date();
+
+export default class OccupancyAveragesComponent extends Component<Args> {
+  @service intl!: any;
 
   days = [0, 1, 2, 3, 4, 5, 6];
   hours = [7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22];
 
-  @tracked day;
-  @tracked title;
+  @tracked day: number | null = null;
+  @tracked title: string | null = null;
 
   get occupancies() {
     return this.args.occupancies || [];
@@ -27,9 +32,7 @@ export default class OccupancyAveragesComponent extends Component {
         return new Date(occupancy.on).getDay() === value;
       });
 
-      const average =
-        occupanciesForDay.reduce((p, c) => p + c.occupancy, 0) /
-          occupanciesForDay.length || 0;
+      const average = occupanciesForDay.reduce((p, c) => p + c.occupancy, 0) / occupanciesForDay.length || 0;
 
       return { value, average };
     });
@@ -45,9 +48,7 @@ export default class OccupancyAveragesComponent extends Component {
           return new Date(occupancy.on).getHours() === value;
         });
 
-      const average =
-        occupanciesForHour.reduce((p, c) => p + c.occupancy, 0) /
-          occupanciesForHour.length || 0;
+      const average = occupanciesForHour.reduce((p, c) => p + c.occupancy, 0) / occupanciesForHour.length || 0;
 
       return { value, average };
     });
@@ -60,35 +61,24 @@ export default class OccupancyAveragesComponent extends Component {
   get listOfNormalizedAverages() {
     if (isNone(this.day)) {
       const averageOccupancySplitByDay = this.averageOccupancySplitByDay;
-      const maxAverage = Math.max.apply(
-        Math,
-        averageOccupancySplitByDay.map(function (o) {
-          return o.average;
-        }),
-      );
+      const maxAverage = Math.max(...averageOccupancySplitByDay.map((o) => o.average));
 
       return averageOccupancySplitByDay.map((entry) => {
-        const normalizedAverage =
-          maxAverage === 0 ? maxAverage : entry.average / maxAverage;
+        const normalizedAverage = maxAverage === 0 ? maxAverage : entry.average / maxAverage;
+        const day = now.setDate(now.getDate() - (now.getDay() || 7) + entry.value);
         return Object.assign(entry, {
           average: normalizedAverage,
-          label: moment.localeData(moment.locale())._weekdaysMin[entry.value],
+          label: this.intl.formatDate(day, { weekday: 'short' }),
         });
       });
     }
 
     if (!isNone(this.day)) {
       const averageOccupancySplitByHour = this.averageOccupancySplitByHour;
-      const maxAverage = Math.max.apply(
-        Math,
-        averageOccupancySplitByHour.map(function (o) {
-          return o.average;
-        }),
-      );
+      const maxAverage = Math.max(...averageOccupancySplitByHour.map((o) => o.average));
 
       return averageOccupancySplitByHour.map((entry) => {
-        const normalizedAverage =
-          maxAverage === 0 ? maxAverage : entry.average / maxAverage;
+        const normalizedAverage = maxAverage === 0 ? maxAverage : entry.average / maxAverage;
 
         return Object.assign(entry, {
           average: normalizedAverage,
@@ -101,23 +91,22 @@ export default class OccupancyAveragesComponent extends Component {
   }
 
   @action
-  clicked(value, event) {
+  clicked(value: null | number, event: Event) {
     if (isNone(value) || !isNone(this.day)) {
       this.day = null;
       this.title = null;
     } else {
+      const day = now.setDate(now.getDate() - (now.getDay() || 7) + value);
+
       this.day = value;
-      this.title = this.intl.t(
-        'occupancy.days.' +
-          moment.localeData('en')._weekdays[value].toString().toLowerCase(),
-      );
+      this.title = this.intl.formatDate(day, { weekday: 'long' });
     }
 
     event.stopPropagation();
   }
 
   @action
-  didInsertBar(element, [value]) {
+  didInsertBar(element: HTMLElement, [value]: [number]) {
     const pixels = value * 32;
 
     element.style.height = pixels <= 1 ? '1px' : pixels + 'px';
---
+++ app/components/occupancy/singular.ts
@@ -0,0 +1,37 @@
+import Component from '@glimmer/component';
+
+interface Args {
+  occupancies: any[];
+}
+
+export default class OccupancySingularComponent extends Component<Args> {
+  get occupancies() {
+    return this.args.occupancies || [];
+  }
+
+  get lastOccupancy() {
+    const lastOccupancy = this.occupancies
+      .map((occupancy) => {
+        return {
+          on: new Date(occupancy.on),
+          occupancy: occupancy.occupancy,
+        };
+      })
+      .sortBy('on')
+      .pop() ?? { on: new Date() };
+
+    return { ...lastOccupancy, ago: -daysBetween(lastOccupancy.on), unit: 'day' };
+  }
+}
+
+function daysBetween(startDate: Date, endDate: Date = new Date()) {
+  // The number of milliseconds in all UTC days (no DST)
+  const oneDay = 1000 * 60 * 60 * 24;
+
+  // A day in UTC always lasts 24 hours (unlike in other time formats)
+  const start = Date.UTC(endDate.getFullYear(), endDate.getMonth(), endDate.getDate());
+  const end = Date.UTC(startDate.getFullYear(), startDate.getMonth(), startDate.getDate());
+
+  // so it's safe to divide by 24 hours
+  return (start - end) / oneDay;
+}
